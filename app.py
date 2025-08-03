ae635f24d4e5d67006a1ebd63e94578e4db9cdb9
Adarsh Paswan <adarhpaswan1106@gmail.com>
2025-08-03 18:23:09 +0530

    Update: improved UI and deployment readiness

--- app.py
+++ app.py
@@ -37,5 +37,6 @@ def index():
     
     return render_template("index.html", prediction=prediction, image_url=image_url)
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=False, host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
+
